819aada5947282b11938989715a1b0663428ff5c
1kirezi <d.ikirezi2@alustudent.com>
2026-05-20 02:52:48 +0200

    new changes

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -2,10 +2,10 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
-	
-		string str=string.Concat("object", "-", "oriented", " ", "programming", " ", "in", " ", "C#");
-		Console.WriteLine(str);
-        }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new string(new char[]{
+            'o','b','j','e','c','t','-','o','r','i','e','n','t','e','d',' ',
+            'p','r','o','g','r','a','m','m','i','n','g',' ','i','n',' ','C','#'}));
+    }
 }
