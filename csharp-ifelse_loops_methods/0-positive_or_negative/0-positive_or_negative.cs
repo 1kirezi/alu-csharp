0ea423b993da7f4369d0aa302cd028635a576c6e
1kirezi <d.ikirezi2@alustudent.com>
2026-05-20 03:01:58 +0200

    new project

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int number = random.Next(-100, 101);
+
+        if (number > 0)
+            Console.WriteLine($"{number} is positive");
+        else if (number < 0)
+            Console.WriteLine($"{number} is negative");
+        else
+            Console.WriteLine($"{number} is zero");
+    }
+}
