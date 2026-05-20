55cf4f771487788b433459d3c408d109957f6f68
1kirezi <d.ikirezi2@alustudent.com>
2026-05-20 02:05:16 +0200

    new project

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 972;
+        Console.Write(number + " Mission Street");
+        Console.WriteLine();
+    }
+}
