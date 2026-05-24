27b17d05a42c37e353732c458d96b0161c7cd957
1kirezi <d.ikirezi2@alustudent.com>
2026-05-25 01:02:09 +0200

    new project

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -1,21 +1,16 @@
-﻿using System;
+﻿int lastDigit = number % 10;
 
-class Program
+Console.Write("The last digit of " + number + " is " + lastDigit + " is ");
+
+if (lastDigit > 5)
 {
-    static void Main(string[] args)
-    {
-        Random random = new Random();
-        int number = random.Next(-10000, 10000);
-        int lastDigit = Math.Abs(number % 10);
-
-        Console.Write($"The last digit of {number} is {lastDigit} and is ");
-
-        if (lastDigit > 5)
-            Console.WriteLine("greater than 5");
-        else if (lastDigit == 0)
-            Console.WriteLine("0");
-        else
-            Console.WriteLine("less than 6 and not 0");
-    }
+    Console.WriteLine("and is greater than 5");
 }
-// This program generates a random integer between -10000 and 10000,
+else if (lastDigit == 0)
+{
+    Console.WriteLine("and is 0");
+}
+else
+{
+    Console.WriteLine("and is less than 6 and not 0");
+}
--- csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -1,19 +1,12 @@
-﻿using System;
+﻿﻿using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         string output = "";
-
         for (int i = 0; i <= 98; i++)
-        {
-            output += i + " = 0x" + i.ToString("x");
-
-            if (i != 98)
-                output += "\n";
-        }
-
+            output += $"{i} = 0x{i:x}\n";
         Console.Write(output);
     }
 }
